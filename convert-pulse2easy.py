38dadc1a71aaf5039ee56d5f762bb2c0c271f7e8
Lukas-Alexander <l.pruski@outlook.com>
2025-10-29 19:13:02 +0100

    Add convert-pulse2easy.py script with project info
    Initial creation of the PulseEffects to EasyEffects preset converter script with project information and licensing details.
    
    Actual Script will be pasted in one of the next commits.

---
+++ convert-pulse2easy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import json
+import sys
+import os
+import copy
+
+# --- Project Information ---
+# Project: PulseEffects to EasyEffects Preset Converter
+# Author: Lukas Alexander Pruski
+# License: MIT License
+# GitHub Repository: https://github.com/[your-username]/pulseeffects-to-easyeffects-converter
+# Copyright (c) 2025 Lukas Pruski. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+### Current State of Development not Pasted here yet. ETA: October 30th, 2025
